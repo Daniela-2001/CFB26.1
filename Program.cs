f35db95ee43aa037ead15df2751d063fee024d69
Daniela Duarte <danielatavaresd@gmail.com>
2024-02-02 08:20:31 +0000

    CFB26.1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace aula26._1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+        static int divide(int dividendo,int divisor, out int resto)
+        {
+            int quociente;
+            quociente= dividendo / divisor;
+            resto = dividendo % divisor;
+            return quociente;
+        }
+    }
+}
